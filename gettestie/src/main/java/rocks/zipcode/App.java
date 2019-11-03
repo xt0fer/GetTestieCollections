4636e6ba1597a882765d0c84a4b2fde08aeeab95
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-11-03 16:27:28 -0500

    Update

--- gettestie/src/main/java/rocks/zipcode/App.java
+++ gettestie/src/main/java/rocks/zipcode/App.java
@@ -1,5 +1,7 @@
 package rocks.zipcode;
 
+import java.util.LinkedList;
+
 /**
  * Hello world!
  */
@@ -12,6 +14,16 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LinkedList<Integer> newList = new LinkedList<>();
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+
+        System.out.println(newList);
+        newList.remove();
+        System.out.println(newList);
+        newList.remove();
+        System.out.println(newList);
+
     }
 }
--- gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
@@ -4,13 +4,44 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.LinkedList;
-import java.util.List;
-
 
 public class LinkedListTest {
 
-    @Test
-    public void Test1(){
-        List<Integer> newList = new LinkedList<>();
+    @Test //LinkedLists have a "First in, First Out" structure.
+    public void removeTest(){
+        LinkedList<Integer> newList = new LinkedList<>();
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+
+        Assert.assertEquals((Integer) 1, newList.get(0));
+        Assert.assertEquals(3, newList.size());
+
+        newList.remove();
+        Assert.assertEquals((Integer) 2, newList.get(0));
+        Assert.assertEquals(2, newList.size());
+
+        newList.remove();
+        Assert.assertEquals((Integer) 3, newList.get(0));
+        Assert.assertEquals(1, newList.size());
+    }
+
+    @Test //LinkedLists have a "First in, First Out" structure.
+    public void removeTest2(){
+        LinkedList<Integer> newList = new LinkedList<>();
+        newList.add(1);
+        newList.add(2);
+        newList.add(3);
+
+        Assert.assertEquals((Integer) 1, newList.get(0));
+        Assert.assertEquals(3, newList.size());
+
+        newList.remove();
+        Assert.assertEquals((Integer) 2, newList.get(0));
+        Assert.assertEquals(2, newList.size());
+
+        newList.remove();
+        Assert.assertEquals((Integer) 3, newList.get(0));
+        Assert.assertEquals(1, newList.size());
     }
 }
