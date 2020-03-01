e0c50d7b0485cc19fa877545c603a1e6b355da42
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-03-01 18:45:54 -0500

    add more Tests for ArrayDeque

--- gettestie/Tests/TestArrayDeque.java
+++ gettestie/Tests/TestArrayDeque.java
@@ -46,4 +46,24 @@ public class TestArrayDeque {
 
         Assert.assertEquals(0, newDeque.getLast().intValue());
     }
+    @Test
+    public void TestArrayDeque4() {
+        ArrayDeque<Integer> newDeque = new ArrayDeque<>();
+        newDeque.add(2876);
+        newDeque.add(2);
+        newDeque.add(1000);
+        newDeque.add(456);
+
+        Assert.assertEquals(456, newDeque.getLast().intValue());
+    }
+    @Test
+    public void TestArrayDeque5() {
+        ArrayDeque<Integer> newDeque = new ArrayDeque<>();
+        newDeque.add(300);
+        newDeque.add(400);
+        newDeque.add(500);
+        newDeque.add(500);
+        Assert.assertEquals(300, newDeque.removeFirst().intValue());
+    }
+
 }
